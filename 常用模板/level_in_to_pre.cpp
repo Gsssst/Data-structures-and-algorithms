e780dda01da28b8e83b533c4831aa8301d646566
Shengt_G <84385603+Gsssst@users.noreply.github.com>
2025-03-04 23:16:03 +0800

    Create level_in_to_pre.cpp

---
+++ 常用模板/level_in_to_pre.cpp
@@ -0,0 +1,31 @@
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+string inorder,levelorder;
+
+void getpreorder(int al,int ar,int bl,int br){
+    int i,j;
+    for(i=bl;i<=br;i++){
+        bool flag = false;
+        for(j=al;j<=ar;j++){
+            if(inorder[j]==levelorder[i]){
+                cout<<levelorder[i];
+                flag = true;
+                break;
+            }
+        }
+        if(flag == true) break;
+    }
+    if(j>al) getpreorder(al,j-1,0,br);
+    if(j<ar) getpreorder(j+1,ar,0,br);
+}
+
+int main(){
+    cin>>inorder>>levelorder;
+    getpreorder(0, inorder.size() - 1, 0, levelorder.size() - 1);
+    
+    return 0;
+}
